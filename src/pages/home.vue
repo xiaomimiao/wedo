27c0bf988f650bcb984c6031cda30a2fc0d3becc
Your Name <you@example.com>
2017-06-05 22:23:31 +0800

    h

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="home">
         <div class="header">
-            <img src="../assets/img/logo.png" alt="">
-            <div class="sou">
+            <img src="../../static/img/logo.png" alt="">
+            <div class="sou" @click="sou()">
                 <input type="text" placeholder="搜索商品名称">
             </div>
+            <span>登录</span>
+        </div>
+        <div class="lunbo">
+            
+        </div>
+        <div class="zaobao">
+            <img src="../../static/img/sczb_ed0a4af.png" alt="">
+            <ul>
+                <!-- <li v-for="msg in data">{{ msg }}</li> -->
+            </ul>
         </div>
     </div>
 </template>
@@ -12,15 +22,23 @@
 <script>
     export default {
         name: "home",
+        data: {
+            // msg
+        },
+        methods: {
+            sou(){
 
+            }
+        }
     }
 
 </script>
 
-<style scope lang="less">
+<style scoped lang="less">
     .home{
         width: 100%;
         .header {
+            position: fixed;
             display: flex;
             top: 0;
             left: 0;
@@ -34,11 +52,44 @@
             margin: 0.15rem 0.15rem 0;
         }
         .header .sou{
-            height: 0.34rem;
+            height: 0.3rem;
             width: 2.99rem;
-            margin-top: 0.07rem;
-            background: url(../assets/img/search_bfba941.png) no-repeat 0 0;
-            background-size: 100%;
+            margin-top: 0.09rem;
+            border-radius: 0.02rem;
+            background: url(../../static/img/search_bfba941.png) no-repeat 0 0;
+            background-size: cover;
+            position: relative;
+            input{
+                border: none;
+                outline: none;
+                font-size: 0.15rem;
+                position: absolute;
+                top: 0.07rem;
+                left: 0.36rem;
+            }
+        }
+        .header span{
+            height: 0.2rem;
+            color: #fff;
+            font-size: 0.16rem;
+            position: relative;
+            top: 0.14rem;
+            right: -0.12rem;
+        }
+
+        .lunbo{
+            height: 2.94rem;
+            background: hotpink;
+        }
+
+        .zaobao{
+            height: 0.46rem;
+            background: red;
+            display: flex;
+            img{
+                height: 0.43rem;
+                width: 0.86rem;
+            }
         }
 
     }
