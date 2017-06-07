831caad9c78c1f0029904d58bb4edd29e1d820a3
Your Name <you@example.com>
2017-06-07 10:52:19 +0800

    ad

--- src/App.vue
+++ src/App.vue
@@ -61,30 +61,23 @@ export default {
   position: absolute;
   top: .05rem;
   left:.4rem;
-  /*background: #f00;*/
-      
+}
 
- }
-  #app .tabbar .class-b p{
-       background: url(../static/img/spr1_5b2e09c.png) no-repeat -0.23rem 0;
-       background-size:100%;
- }
- 
   #app .tabbar a:nth-of-type(1) p{
        background: url(../static/img/spr1_5b2e09c.png) no-repeat 0 -0.23rem;
        background-size:100%;
  }
- 
-   #app .tabbar a:nth-of-type(2) p{
-    background: url(../static/img/spr1_5b2e09c.png) no-repeat 0 -0.69rem;
-    background-size:100%; 
+   
+  #app .tabbar a:nth-of-type(2) p{
+		    background: url(../static/img/spr1_5b2e09c.png) no-repeat 0 -0.69rem;
+		    background-size:100%; 
   }
    #app .tabbar a:nth-of-type(3) p{
-    background: url(../static/img/spr1_5b2e09c.png) no-repeat 0 -1.15rem;
-    background-size:100%; 
+	  	  background: url(../static/img/spr1_5b2e09c.png) no-repeat 0 -1.15rem;
+			  background-size:100%; 		  
   }
    #app .tabbar a:nth-of-type(4) p{
-    background: url(../static/img/spr1_5b2e09c.png) no-repeat 0 -1.61rem;
-    background-size:100%; 
-    }
+		    background: url(../static/img/spr1_5b2e09c.png) no-repeat 0 -1.61rem;
+		    background-size:100%; 
+  }
  </style>
