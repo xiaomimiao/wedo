5c3af1bde80a3f23cb8871e6c1d3a64421b5a5ed
Your Name <you@example.com>
2017-06-07 19:35:32 +0800

    market

--- src/pages/market.vue
+++ src/pages/market.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="market">
       <div  class="market_head">
+      <img src="../../static/img/icon_back_n.png" alt=""  @click="hui()">
           <h2>商品分类</h2>
+          <img src="../../static/img/search.png" alt="" @click="sou()">
       </div>
-      <div class="market_left">
+      <div class="market_left" :style="{clear: 'both'}">
           <ul>
 
               <li  v-for="(item,index) in data.data"  @click="change_list(item.category_id)" :style="i == item.category_id ? { color: '#fb7d34',fontSize:'0.2rem' } : { color: '#333',fontSize:'0.1562rem' }"  >{{ item.category_name }}</li>
@@ -79,7 +81,13 @@
             path:'/detial'
           })
 
-       }
+       },
+      hui(){
+        this.$router.push({path:'/home'})
+      },
+      sou(){
+        this.$router.push({path:'/sou'})
+      },
     },
     created(){
         this.axios.get('../static/xiaomi data/category_v2.json').then(res =>{
@@ -93,19 +101,34 @@
 
 <style scoped>
 
-  .market_head{
-    width:100%;
-    height:0.483rem;
-    line-height:0.483rem;
-    text-align:center;
-    background:url(../../static/img/icon_back_n.png) 0.15rem center no-repeat ,url(../../static/img/search.png) no-repeat 3.7rem center #f2f2f2;;
-    background-size:0.253rem 0.253rem;
-  }
-  .market_head h2{
-    font-weight: 400;
-    font-size:0.1562rem;
-    color:#666;
-  }
+.market_head{
+  width:100%;
+  height:0.483rem;
+  line-height:0.483rem;
+  background:#f2f2f2;
+  z-index:2;
+}
+.market_head img:first-of-type{
+  float:left;
+  height:0.253rem;
+  width:0.254rem;
+  margin:0.15rem;
+}
+.market_head h2{
+  font-weight: 400;
+  font-size:0.1562rem;
+  color:#666;
+  float:left;
+  text-indent:1rem;
+
+}
+.market_head img:last-of-type{
+  height:0.253rem;
+  width:0.254rem;
+  float:left;
+  margin:0.15rem 0 0 1.5rem;
+}
+
   .market_left{
     float:left;
     background-color:#fefefe;
